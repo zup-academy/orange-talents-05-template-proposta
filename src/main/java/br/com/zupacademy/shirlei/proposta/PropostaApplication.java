6d4c45cf20589708cdeccfd224de1d39b4510d8f
Shirlei <schiufreitas@gmai.com>
2021-06-03 20:57:06 -0300

    Setup

---
+++ src/main/java/br/com/zupacademy/shirlei/proposta/PropostaApplication.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.shirlei.proposta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropostaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropostaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zupacademy/shirlei/proposta/PropostaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.shirlei.proposta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
