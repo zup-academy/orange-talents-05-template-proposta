5a10c46a54919960365cfb1c645ddfa2e2e4153c
Shirlei <schiufreitas@gmai.com>
2021-06-16 22:07:11 -0300

    exeptions

--- dockerfile
+++ dockerfile
@@ -1,5 +1,5 @@
 FROM openjdk:11
 MAINTAINER Shirlei Machado
 ARG JAR_FILE=target/*.jar
-COPY ${JAR_FILE} container-java.jar
-ENTRYPOINT ["java", "-jar", "container-java.jar"]
+COPY ${JAR_FILE} proposta-app.jar
+ENTRYPOINT ["java", "-jar", "proposta-app.jar"]
---
+++ src/main/java/br/com/zupacademy/shirlei/proposta/exception/ErroApi.java
@@ -0,0 +1,25 @@
+package br.com.zupacademy.shirlei.proposta.exception;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_NULL)
+public class ErroApi {
+
+    private String campo;
+    private String erro;
+
+    public ErroApi(String campo, String erro) {
+        this.campo = campo;
+        this.erro = erro;
+    }
+
+    public String getCampo() {
+        return campo;
+    }
+
+    public String getErro() {
+        return erro;
+    }
+
+}
