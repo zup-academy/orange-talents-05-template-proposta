7f4731f08b2d5623a07d4099b54d3111fa284eb9
leonardoinatel <leonardodamasceno@gec.inatel.br>
2021-06-19 15:28:51 -0300

    065.como_saber_tudo_funcionando_corretamente.md

--- proposta/src/main/java/com/orange5/proposta/security/SecurityConfiguration.java
+++ proposta/src/main/java/com/orange5/proposta/security/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.orange5.proposta.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
@@ -15,7 +16,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests(authorizeRequest -> authorizeRequest.anyRequest().authenticated())
+        http.csrf().disable().authorizeRequests(authorizeRequest -> authorizeRequest
+                .antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+                .anyRequest().authenticated())
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
 
